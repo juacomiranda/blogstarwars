47af8665cbc48d8d9a964fa036c4b8a5b83efdf8
juacomiranda <jmiranda@live.cl>
2026-05-24 12:34:57 -0400

    sistema grid para imagenes

--- src/pages/personajes.jsx
+++ src/pages/personajes.jsx
@@ -1,7 +1,21 @@
 export const Personajes = () => {
     return (
-        <div className="text-center mt-5">
+        <div className="text-center mt-5 text-light">
             <h1>Personajes</h1>
+                    <div className="row text-center mt-5">
+                        <div className="col-3">
+                            Hola
+                        </div>
+                        <div className="col-3">
+                            como
+                        </div>
+                        <div className="col-3">
+                            estas
+                        </div>
+                        <div className="col-3">
+                            nuevo
+                        </div>
+                    </div>
         </div>
     );
 };
