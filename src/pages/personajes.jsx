57f42c31f5349569e8da02bc21b74e806c1fe003
juacomiranda <jmiranda@live.cl>
2026-05-27 09:05:09 -0400

    personajes conectado a la api

--- src/pages/personajes.jsx
+++ src/pages/personajes.jsx
@@ -1,21 +1,34 @@
+import { useEffect } from "react"
+import useGlobalReducer from "../hooks/useGlobalReducer"
+
 export const Personajes = () => {
+    const { store, dispatch } = useGlobalReducer()
+
+    useEffect(() => {
+        fetch("https://www.swapi.tech/api/people/")
+            .then(res => res.json())
+            .then(data => dispatch({ type: "set_personajes", payload: data.results }))
+    }, [])
+
     return (
         <div className="text-center mt-5 text-light">
             <h1>Personajes</h1>
-                    <div className="row text-center mt-5">
-                        <div className="col-3">
-                            Hola
-                        </div>
-                        <div className="col-3">
-                            como
-                        </div>
-                        <div className="col-3">
-                            estas
-                        </div>
-                        <div className="col-3">
-                            nuevo
+            <div className="row text-center mt-5 g-4">
+                {store.personajes.map((personaje, i) => (
+                    <div className="col-3" key={i}>
+                        <div className="card bg-dark text-light h-100">
+                            <img
+                                src="https://placehold.co/300x200?text=Personaje"
+                                className="card-img-top"
+                                alt={personaje.name}
+                            />
+                            <div className="card-body">
+                                <h5 className="card-title">{personaje.name}</h5>
+                            </div>
                         </div>
                     </div>
+                ))}
+            </div>
         </div>
     );
 };
--- src/store.js
+++ src/store.js
@@ -1,32 +1,14 @@
 export const initialStore=()=>{
   return{
-    message: null,
-    todos: [
-      {
-        id: 1,
-        title: "Make the bed",
-        background: null,
-      },
-      {
-        id: 2,
-        title: "Do my homework",
-        background: null,
-      }
-    ]
+    personajes: []
   }
 }
 
 export default function storeReducer(store, action = {}) {
   switch(action.type){
-    case 'add_task':
-
-      const { id,  color } = action.payload
-
-      return {
-        ...store,
-        todos: store.todos.map((todo) => (todo.id === id ? { ...todo, background: color } : todo))
-      };
+    case 'set_personajes':
+      return { ...store, personajes: action.payload }
     default:
       throw Error('Unknown action.');
-  }    
+  }
 }
